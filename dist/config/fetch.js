c52e8ff9f2a64b9e30512b23cf6e85d5e72c1e5a
Robert Bornschein <Robert.bornschein@mailbox.tu-dresden.de>
2021-03-11 06:35:17 +0100

    Delete dist directory

--- dist/config/fetch.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.fetch = void 0;
-const common_1 = require("@bothaven/common");
-const optionsBuilder = new common_1.OptionsBuilder();
-exports.fetch = optionsBuilder.fetch;
-//# sourceMappingURL=fetch.js.map
